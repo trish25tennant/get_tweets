e3719d514629b027e8583a4e27be3456e074a6da
trish25tennant <53783902+trish25tennant@users.noreply.github.com>
2019-08-20 15:58:25 +0200

    Update get_tweets.py

--- get_tweets.py
+++ get_tweets.py
@@ -25,6 +25,19 @@ def get_tweets(username):
 	number_of_tweets = 100
 
 	#get tweets
+	tweets_for_csv = []
+	for tweet in tweepy.Cursor(api.user_timeline, screen_name = username).items(number_of_tweets):
+        #create array of tweet information: username, tweet id, date/time, text
+		tweets_for_csv.append([username, tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")])
+
+	#write to a new csv file from the array of tweets
+	outfile = username + "_tweets.csv"
+	print outfile
+	with open(outfile, 'w+') as file:
+		writer = csv.writer(file, delimiter=',')
+		writer.writerows(tweets_for_csv)
+
+
 	
 
 #if we're running this as a script
